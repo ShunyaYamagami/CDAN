5e710d45da8de07dad128aaa77093e1f8a296d88
ShunyaYamagami <4623533@ed.tus.ac.jp>
2023-08-19 13:17:49 +0900

    modify

--- pytorch/do_memo.sh
+++ pytorch/do_memo.sh
@@ -1,12 +1,7 @@
-. cmd.sh 0 0 0_2_4   \
+cd  ~/lab/gda/da/CDAN/pytorch
+
+. cmd.sh 0 0 -1   \
     -p OfficeHome  \
     --method CDAN+E  \
     --task   contrastive_rpl_dim128_wght0.6_AE_bs512_ep3000_outd64_g3  \
     --tmux   CDAN+E_0
-
-. cmd.sh 1 0 1_3_5 \
-    -p OfficeHome  \
-    --method CDAN+E  \
-    --task   contrastive_rpl_dim128_wght0.6_AE_bs512_ep3000_outd64_g3  \
-    --tmux   CDAN+E_1
-
